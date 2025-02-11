03c5c4875563e5d479b30ab4c59d9b6d9d1553e6
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2025-02-11 23:28:21 +0000

    Made queries nicer

--- query_db.py
+++ query_db.py
@@ -6,28 +6,32 @@ engine = sqlalchemy.create_engine("sqlite+pysqlite:///pupils.sqlite", echo=False
 
 
 with (orm.Session(engine) as session):
-    print('Get all houses')
-    houses = session.query(model.House)
+    print('Get all houses\n')
+    houses = session.query(model.House).all()
 
-    for house in houses:
-        print(house)
+    print(houses)
 
-    print('Get the ORM object for maths')
+    print('\nGet the ORM object for maths\n')
 
     maths = session.query(model.Subject).where(model.Subject.name == "Mathematics").one()
 
     print(maths)
 
-    print('List all students of maths')
+    print('\nList all students of maths\n')
 
     print(maths.pupils)
 
-    print('Get the ORM object for Queensgate')
+    print('\nGet the ORM object for Queensgate\n')
 
     house_qg = session.query(model.House).where(model.House.name == "Queensgate").one()
 
     print(house_qg)
 
-    print('List all students in Queensgate')
+    print('\nList all students in Queensgate\n')
 
     print(house_qg.pupils)
+
+    print('\nFind all students who study maths\n')
+
+    maths_pupils = session.query(model.Pupil).join(model.PupilSubject).join(model.Subject).filter(model.Subject.name == "Mathematics").all()
+    print(maths_pupils)
