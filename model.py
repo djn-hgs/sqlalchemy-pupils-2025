7e019d9bea4a4c175d5ca111c834febacd255dd9
Daryl Noyce <daryl.noyce@highgateschool.org.uk>
2025-02-11 21:19:33 +0000

    Updating relationships

--- model.py
+++ model.py
@@ -1,22 +1,23 @@
 import sqlalchemy
 import sqlalchemy.orm as orm
 
-class Base(orm.DeclarativeBase):
+class Base(orm.DeclarativeBase, orm.MappedAsDataclass):
     pass
 
 class Pupil(Base):
     __tablename__ = "pupil"
-    id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    id: orm.Mapped[int] = orm.mapped_column(init=False, primary_key=True, repr=False)
     first_name: orm.Mapped[str] = orm.mapped_column()
     last_name: orm.Mapped[str] = orm.mapped_column()
-    house_id: orm.Mapped[int] = orm.mapped_column(sqlalchemy.ForeignKey('house.house_id'))
+    house_id: orm.Mapped[int] = orm.mapped_column(sqlalchemy.ForeignKey('house.house_id'), repr=False)
 
+    house: orm.Mapped['House'] = orm.relationship(backref="pupils")
 
 class House(Base):
     __tablename__ = "house"
-    house_id: orm.Mapped[int] = orm.mapped_column(primary_key=True)
+    house_id: orm.Mapped[int] = orm.mapped_column(init=False, primary_key=True, repr=False)
     name: orm.Mapped[str] = orm.mapped_column()
 
-    pupils = orm.relationship(Pupil, backref='house')
+    pupils: orm.Mapped[list[Pupil]] = orm.relationship(default_factory=list)
 
 
